89f6c42bdf2fb482896eda15fb1133a1fe12897d
Yohanes Eka Adhi Setiawan <yohanesekaadhisetiawan@Yohaness-Mac-mini.local>
2026-05-16 03:10:07 +0700

    Bridge PWA icon generation to npm package

--- src/Console/Generator/PwaIconsCommand.php
+++ src/Console/Generator/PwaIconsCommand.php
@@ -7,19 +7,17 @@ use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
 use Symfony\Component\Process\Process;
 
-#[Signature('accelerator:generate-pwa-icons')]
+#[Signature('accelerator:generate-pwa-icons {source=public/favicon.svg : Source icon path} {--preset=minimal : @vite-pwa/assets-generator preset}')]
 #[Description('Generate PWA icon assets from public/favicon.svg')]
 class PwaIconsCommand extends Command
 {
     public function handle(): int
     {
-        $process = new Process([
-            'npx',
-            'pwa-assets-generator',
-            '--preset',
-            'minimal',
-            'public/favicon.svg',
-        ]);
+        $localBinary = base_path('node_modules/.bin/laravel-pwa');
+        $process = new Process(file_exists($localBinary)
+            ? [$localBinary, 'icons', '--source', $this->argument('source'), '--preset', $this->option('preset')]
+            : ['npx', '--yes', '@wireninja/vite-plugin-laravel-pwa', 'icons', '--source', $this->argument('source'), '--preset', $this->option('preset')]
+        );
 
         $process->setTty(Process::isTtySupported());
         $process->run(function (string $type, string $buffer): void {
